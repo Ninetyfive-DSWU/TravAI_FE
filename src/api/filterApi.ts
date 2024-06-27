0126066a39a3ebe6a7aef69efe1be2246cb9c410
DAASHeo <hibstar719@naver.com>
2024-06-27 14:31:07 +0900

    feat : 필터링 엔드포인트 생성 및  연결

--- src/api/filterApi.ts
+++ src/api/filterApi.ts
@@ -1,1 +1,30 @@
 import { API_BASE_URL } from "@constants/Constants";
+interface FilteredData {
+  place: string;
+  companions: string;
+  nights: number;
+  theme: string;
+  startdate: string;
+  enddate: string;
+}
+
+export const Filtering = async (filteredData: FilteredData) => {
+  const jsonFilteredData = JSON.stringify(filteredData);
+  try {
+    const response = await fetch(`${API_BASE_URL}/make_prompt/`, {
+      method: "POST",
+      headers: {
+        Authorization: `Token ${localStorage.getItem("token")}`,
+        "Content-Type": "application/json",
+      },
+      body: jsonFilteredData,
+    });
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
