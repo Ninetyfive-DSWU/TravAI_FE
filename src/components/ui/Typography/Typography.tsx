f4c79885e5b3666e59c766b26de8cd9d76f81df5
SH-indeyong <ohsh1115@gmail.com>
2024-07-11 18:54:43 +0900

    Style: 장소 텍스트 스타일 변경

--- src/components/ui/Typography/Typography.tsx
+++ src/components/ui/Typography/Typography.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import styled from 'styled-components';
-import pxToVw from '@utils/PxToVw';
+import React from "react";
+import styled from "styled-components";
+import pxToVw from "@utils/PxToVw";
 
 interface TypoProps {
   content: string;
@@ -26,7 +26,8 @@ interface StyledSpanProps {
 }
 
 const StyledSpan = styled.span<StyledSpanProps>`
-  font-size: ${({ $size }) => ($size ? pxToVw($size) : 'inherit')};
-  font-weight: ${({ $fontWeight }) => ($fontWeight ? `${$fontWeight}` : 'inherit')};
-  color: ${({ $color }) => ($color ? `${$color}` : 'inherit')};
+  text-align: center;
+  font-size: ${({ $size }) => ($size ? pxToVw($size) : "inherit")};
+  font-weight: ${({ $fontWeight }) => ($fontWeight ? `${$fontWeight}` : "inherit")};
+  color: ${({ $color }) => ($color ? `${$color}` : "inherit")};
 `;
